557c774d1894c770f5836feb0ff178c7f99efcbe
EC2 Default User <ec2-user@ip-172-31-24-7.us-east-2.compute.internal>
2019-09-23 00:38:46 +0000

    atualizando codigo da busca de produtos

--- controller/indexcontroller.js
+++ controller/indexcontroller.js
@@ -7,13 +7,14 @@ class IndexController{
         //console.log(JSON.stringify(listProduct, null, 4));
         res.render('index2', { listProduct: listProduct });
     }
+    
     async search(req,res,next){
         var search = req.body.search;
         var listProduct  = null;
         if(search ===""){
             listProduct  = await productService.getAll();
         }else{
-            listProduct  = await productService.getAllBySearch(req.body.search);
+            listProduct  = await productService.getAllBySearch(search);
         }
         res.render('index2', { listProduct: listProduct });
     }
--- services/productservice.js
+++ services/productservice.js
@@ -3,7 +3,6 @@ var {
 } = require('@aws/dynamodb-data-mapper');
 var ConditionExpression = require('@aws/dynamodb-expressions');
 var {contains} = require('@aws/dynamodb-expressions');
-
 var DynamoDB = require('aws-sdk/clients/dynamodb');
 
 const client = new DynamoDB({region: 'us-east-2'});
@@ -11,6 +10,17 @@ const mapper = new DataMapper({client});
 const Product = require("../models/product");
 
 class ProductService{
+    async getAllBySearch(search){
+        var list = [];
+        var result = await mapper.scan(Product,{limit:5, filter: {
+                                                ...contains(search),
+                                                subject: 'name'
+                                            }});
+        for await (const item of result) {
+            list.push(item);
+        }
+        return list;
+   }
    async getAll(){
         var list = [];
 
@@ -21,18 +31,7 @@ class ProductService{
         return list;
 
    } 
-   async getAllBySearch(search){
-       var list = [];
-
-        var result = await mapper.scan(Product,{limit:5, filter: {
-                                                ...contains(search),
-                                                subject: 'name'
-                                            }});
-        for await (const item of result) {
-            list.push(item);
-        }
-        return list;
-   }
+   
 }
 module.exports = ProductService;
 /*colinha
